be35d3c9bd569699cc12bb5d387359e8ba204ae2
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2022-08-17 15:59:03 -0400

    Widgets visible

---
+++ app.js
@@ -0,0 +1,103 @@
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <h1> Commence Dashboard Creation! </h1>
+//         )
+//     }
+// }
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Dashboard</h3>
+                <ul>
+
+                    <li>Widget</li>
+                    <li>Reviews</li>
+                    <li>Customers</li>
+                    <li>Online Analysis</li>
+                    <li>Settings</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class Reviews extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Reviews</h3>
+                <p>1,281</p>
+            </div>
+        )
+    }
+}
+
+class AvgRating extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Average Rating</h3>
+                <p>4.6</p>
+            </div>
+        )
+    }
+}
+
+class SentAn extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Sentiment Analysis</h3>
+                <ul>
+                    <li>960</li>
+                    <li>122</li>
+                    <li>321</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class WebVisitors extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Website Visitors</h3>
+                <p>821</p>
+                <div></div>
+            </div>
+        )
+    }
+}
+
+class Stats extends React.Component {
+    render() {
+        return (
+            <div>
+                <Reviews />
+                <AvgRating />
+                <SentAn />
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Sidebar />
+                <Stats />
+                <WebVisitors />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('.main')
+)
